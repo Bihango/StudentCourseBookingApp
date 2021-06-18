9d395e826c441bae2f5dbf9d052bd790209d5248
Bihango <bihango.sarkar@gmail.com>
2021-06-18 07:12:31 -0400

    First Commit!
    Completed:
    -Admin Login
    
    Not Implemented:
    -Register
    -Course Manage

---
+++ StudentCourseBookingApp/app/src/main/java/com/example/studentcoursebookingapp/manageCourse.java
@@ -0,0 +1,14 @@
+package com.example.studentcoursebookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class manageCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_course);
+    }
+}
